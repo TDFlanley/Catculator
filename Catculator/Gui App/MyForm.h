44d03c071a7bdb4ee828cb4ca05feb06dbae08c4
TDFlanley <142453753+TDFlanley@users.noreply.github.com>
2023-08-16 15:10:41 -0700

    Add files via upload
    GUI calculator uses C++ and .NET Framework to perform addition, subtraction, multiplication, division, exponent, square root and cube root calculation.

---
+++ Catculator/Gui App/MyForm.h
@@ -0,0 +1,270 @@
+#include <math.h>
+#pragma once
+
+namespace GuiApp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(12, 39);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(116, 40);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->textBox2->Location = System::Drawing::Point(236, 39);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(116, 40);
+			this->textBox2->TabIndex = 1;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(138, 82);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 41);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"+";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->button2->Location = System::Drawing::Point(138, 129);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 41);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"-";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->Location = System::Drawing::Point(138, 176);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 41);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"x";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->button4->Location = System::Drawing::Point(138, 223);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 41);
+			this->button4->TabIndex = 6;
+			this->button4->Text = L"/";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(255, 150);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(104, 33);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Result";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+
+			// 
+			// button5
+			// 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->Location = System::Drawing::Point(138, 270);
+			this->button5->Margin = System::Windows::Forms::Padding(2);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 37);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"^";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// button6
+			// 
+			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button6->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->button6->Location = System::Drawing::Point(138, 312);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(75, 38);
+			this->button6->TabIndex = 9;
+			this->button6->Text = L"sqrt";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &MyForm::button6_Click);
+			// 
+			// button7
+			// 
+			this->button7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button7->Location = System::Drawing::Point(138, 356);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(75, 36);
+			this->button7->TabIndex = 10;
+			this->button7->Text = L"cbrt";
+			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &MyForm::button7_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(975, 749);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"CATculator V1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text) + System::Convert::ToInt16(textBox2->Text);
+
+		label2->Text = System::Convert::ToString(output);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text) - System::Convert::ToInt16(textBox2->Text);
+
+		label2->Text = System::Convert::ToString(output);
+}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text) * System::Convert::ToInt16(textBox2->Text);
+
+		label2->Text = System::Convert::ToString(output);
+}
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text) / System::Convert::ToInt16(textBox2->Text);
+
+		label2->Text = System::Convert::ToString(output);
+}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (label2->Text == "69")
+			label2->Text = System::Convert:: ToString("Nice");
+}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	int output = pow(System::Convert::ToInt16(textBox1->Text) , System::Convert::ToInt16(textBox2->Text));
+
+	label2->Text = System::Convert::ToString(output);
+}
+private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e) {
+	int output = sqrt(System::Convert::ToInt16(textBox1->Text));
+
+	label2->Text = System::Convert::ToString(output);
+}
+private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+	int output = cbrt(System::Convert::ToInt16(textBox1->Text));
+
+	label2->Text = System::Convert::ToString(output);
+}
+};
+}
